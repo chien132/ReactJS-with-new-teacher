bfb41bdb633ea73e3a9606fe43ba4d6487ad671e
Chien Dang <44261143+chien132@users.noreply.github.com>
2021-07-14 01:17:32 +0700

    03 React Basics & Working With Components

---
+++ 01-starting-setup/src/components/Expenses/ExpenseDate.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./ExpenseDate.css";
+
+function ExpenseDate(props) {
+  const day = props.date.toLocaleString("vi-vn", { day: "2-digit" });
+  const month = props.date.toLocaleString("vi-vn", { month: "long" });
+  const year = props.date.getFullYear();
+  return (
+    <div>
+      <div className="expense-date">
+        <div className="expense-date__day">{day}</div>
+        <div className="expense-date__month">{month}</div>
+        <div className="expense-date__year">{year}</div>
+      </div>
+    </div>
+  );
+}
+export default ExpenseDate;
